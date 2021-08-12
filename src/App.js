4c88854689ef94cd2074d7cfdfb39a5d98657be9
Ishan Manandhar <ishanmanandhar@lftechnology.com>
2021-08-12 15:39:21 +0545

    configured chakra into the application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container mt="10" maxW="xl" centerContent>
+        <Box bg="green.500" w="100%" p={4} color="white">
+          ChakraUI is working!!
+        </Box>
+      </Container>
     </div>
   );
 }
